ffb047c01fe0003102aae92c2b1677694a8f3352
vladkoduc <142478161+vladkoduc@users.noreply.github.com>
2024-12-20 13:35:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const rectangle4 = document.getElementById('rectangle4');
+    const rectangle5 = document.getElementById('rectangle5');
+    if (rectangle4) {
+        rectangle4.addEventListener('click', function() {
+            window.location.href = 'https://t.me/+G6VEmzIn7Tw0MGQy'; 
+        });
+    }
+    if (rectangle5) {
+        rectangle5.addEventListener('click', function() {
+            window.location.href = 'https://t.me/lichkavlada'; 
+        });
+    }
+});
---
+++ logic-lowmn-fixed-base.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', () => {const stableHeight = Telegram.WebApp.viewportStableHeight;document.querySelector('.content').style.height = `${stableHeight - 60}px`;});
